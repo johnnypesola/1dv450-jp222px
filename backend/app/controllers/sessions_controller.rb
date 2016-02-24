63a9131daa2bbb48b79eb7b8c4dd37c3f0a3fdff
Johnny Pesola <johnny@pesola.eu>
2016-02-24 18:52:22 +0100

    OAuth refactored and tested

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -6,17 +6,15 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
 
-    clientuser = Clientuser.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Clientuser.create_with_omniauth(auth["provider"], auth["uid"])
+    # Check if users exists in DB, else create user from github data
+    clientuser = Clientuser.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Clientuser.create_with_omniauth(auth)
 
-    clientuser.name = auth["info"]["name"] # could be updated (better check when new logins)
-    clientuser.token = auth["credentials"]["token"] # github provider token should not give out
-    clientuser.auth_token = SecureRandom.urlsafe_base64(nil, false) # generate a access_key for client to call the API
-    clientuser.token_expires = Time.now + 1.hour
-    clientuser.save
+    # Update some specific client user info
+    clientuser.update_info(auth)
 
-    # read back the session
-    url = session[:client_call]
-    session[:client_call] = nil
+    # Read back the client callback url from session
+    url = session[:client_callback]
+    session[:client_callback] = nil
     redirect_to  "#{url}?auth_token=#{clientuser.auth_token}&token_expires=#{Rack::Utils.escape(clientuser.token_expires.to_s)}"
   end
 
@@ -27,32 +25,22 @@ class SessionsController < ApplicationController
   end
 
   def authenticate
-    session[:client_call] = params[:callback]
+    session[:client_callback] = params[:callback]
     redirect_to "/auth/github"
   end
 
-
-
-  # This is just for testing stuff
   def test
-    # check for the headers
-    auth_token = request.headers["X-auth-token"] || nil
-    clientuser = nil
-    unless auth_token.nil?
-      # should test against expires-date also
-      clientuser = Clientuser.find_by_auth_token(auth_token) || nil
-    end
 
-    if(clientuser.nil?)
+    user = Clientuser.try_get_logged_in_user(request.headers)
+    
+    if user.nil?
       response.status = 401
       render :nothing => true
     else
       response.status = 200
-      render :json => clientuser
+      render :json => user
     end
 
-
   end
-
 
 end
--- backend/app/models/clientuser.rb
+++ backend/app/models/clientuser.rb
@@ -1,10 +1,50 @@
 class Clientuser < ActiveRecord::Base
 
-  def self.create_with_omniauth(provider, uid)
+  def self.create_with_omniauth(auth)
+
+    # Create user from github auth info
     create! do |clientuser|
-      clientuser.provider = provider
-      clientuser.uid = uid
+
+      clientuser.provider = auth["provider"]
+      clientuser.uid = auth["uid"]
+
     end
   end
 
+  def update_info(auth)
+
+    # Update with new info from github auth info
+    self.name = auth["info"]["name"] # could be updated (better check when new logins)
+    self.token = auth["credentials"]["token"] # github provider token should not give out
+    self.auth_token = SecureRandom.urlsafe_base64(nil, false) # generate a access_key for client to call the API
+    self.token_expires = Time.now + 1.hour
+
+    self.save
+
+  end
+
+  def has_login_expired?
+    self.token_expires < Time.now
+  end
+
+  def self.try_get_logged_in_user(headers)
+
+    # check for the headers
+    auth_token = headers["X-auth-token"] || nil
+    clientuser = nil
+    unless auth_token.nil?
+
+      # Check if user exists
+      clientuser = Clientuser.find_by_auth_token(auth_token) || nil
+
+    end
+
+    # Return user if user exists and has valid auth session
+    unless clientuser.nil? || clientuser.has_login_expired?
+      return clientuser
+    end
+
+    return nil
+
+  end
 end
