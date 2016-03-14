a29a506f4a22994f52b7a74f259c3fc1c23959cc
Johnny Pesola <johnny@pesola.eu>
2016-03-14 01:09:43 +0100

    Installed frontend boilerplate

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name climbingReportApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the climbingReportApp
+ */
+angular.module('climbingReportApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
