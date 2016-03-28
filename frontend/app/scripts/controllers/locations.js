26ad1a8697bbd0eaec8c13a2f42352989564ce5d
Johnny Pesola <johnny@pesola.eu>
2016-03-28 06:43:57 +0200

    Now possible to add locations, still needs work

--- frontend/app/scripts/controllers/locations.js
+++ frontend/app/scripts/controllers/locations.js
@@ -12,6 +12,7 @@ angular.module('climbingReportApp')
 
     // Init vars START
 
+    var locationsData = {};
     var isLoggedIn = AuthService.isLoggedIn();
     $scope.pageNum = 1;
     $scope.locationsPerPage = 4;
@@ -29,14 +30,18 @@ angular.module('climbingReportApp')
 
       if(isLoggedIn){
 
-        $scope.locationsData = Location.query({
+        locationsData = Location.query({
           page_num: $scope.pageNum,
           per_page: $scope.locationsPerPage,
           sort_by: 'created_at',
           sort_order: 'asc'
         });
 
-        $scope.locationsData.$promise
+        locationsData.$promise
+
+          .then(function(response){
+            $scope.visibleLocations = response.items;
+          })
 
           // If locations could not be fetched.
           .catch(function(){
@@ -71,6 +76,65 @@ angular.module('climbingReportApp')
     };
 
     // Private methods END
+
+    // Public methods START
+
+    $scope.setIsAddMode = function(isAddMode){
+
+      $scope.isAddMode = isAddMode;
+
+      if(isAddMode) {
+
+        $scope.visibleLocations = [];
+        $scope.newLocation = [
+          {
+            name: '',
+            latitude: $scope.mapValues.center.latitude,
+            longitude: $scope.mapValues.center.longitude
+          }
+        ];
+
+      } else {
+
+        $scope.visibleLocations = locationsData.items;
+        $scope.newLocation = [];
+      }
+    };
+
+    $scope.addLocation = function(location){
+
+      location.isBusy = true;
+
+      var locationToAdd = new Location(
+        {
+          name: location.name,
+          latitude: location.latitude,
+          longitude: location.longitude
+        }
+      );
+
+      locationToAdd.$save()
+
+        .then(function(response){
+
+          $scope.isAddMode = false;
+          location.isBusy = false;
+
+          $scope.visibleLocations.push(response.items[0]);
+        })
+
+        // If location could not be added.
+        .catch(function() {
+
+          // Set Flash message
+          $rootScope.FlashMessage = {
+            type: 'danger',
+            message: 'Something strange happened. Location could not be added.'
+          };
+        });
+    };
+
+    // Public methods END
 
     // Init code START
 
